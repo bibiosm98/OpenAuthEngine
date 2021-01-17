e929699c6db51f726257a83622ed9bd0a8da5fa9
Norbert <bibiosm98@gmail.com>
2021-01-17 18:11:36 +0100

    initial

---
+++ routes/index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+
+router.get('/', async (req, res) => { 
+    res.render('layouts/login');
+});
+
+router.get('/verify', async (req, res) => {
+    res.render('layouts/verify');
+});
+
+router.get('/success', async (req, res) => {
+    res.render('layouts/success');
+}); 
+
+
+router.post('/signin', async (req, res) => {
+    let api_send_sms = 'https://open-auth-engine.herokuapp.com/api/send_sms'
+    return new Promise((resolve, reject) => {
+        let reqBody = {
+            // "auth_token": req.body.token,
+            "auth_token": process.env.TOKEN,
+            "user_number": req.body.number
+        }; 
+        // console.log(api_send_sms);
+        // console.log(reqBody);
+        try{
+            request.post({ 
+                    uri: api_send_sms,
+                    json: true,
+                    body: reqBody
+                },
+                (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    if(body=="SMS SENT"){
+                        // console.log("IF SENT");
+                        res.redirect('/verify');
+                    }else{
+                        res.redirect('/');
+                    }
+                }else{
+                    res.redirect('/');
+                }
+            })
+        }
+        catch(e){ 
+            res.redirect('/');
+            reject('Error while POST token and number')
+        }
+    })
+});
+
+router.post('/verify', async (req, res) => {
+    let api_send_sms = 'https://open-auth-engine.herokuapp.com/api/verify_sms'
+    return new Promise((resolve, reject) => {
+        let reqBody = {
+            // "auth_token": req.body.token,
+            "auth_token": process.env.TOKEN,
+            "user_number": req.body.number,
+            "user_verification_code": req.body.verify_code
+        }; 
+        // console.log(api_send_sms);
+        // console.log(reqBody);
+        try{
+            request.post({ 
+                    uri: api_send_sms,
+                    json: true,
+                    body: reqBody
+                },
+                (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    // console.log(body);
+                    // console.log(error);
+                    if(body=="VERIFIED SUCCESSFULLY"){
+                        // console.log("VERIFIED SUCCESSFULLY");
+                        res.redirect('/success');
+                    }else if(body=="WRONG CODE"){
+                        // console.log("WRONG CODE");
+                        res.redirect('/verify');
+                    }else if(body=="UNAUTHORIZED"){
+                        // console.log("UNAUTHORIZED");
+                        res.redirect('/');
+                    }else{
+                        res.redirect('/');
+                    }
+                }else{
+                    res.redirect('/');
+                    // res.render('layouts/newView', "Błedne dane");
+                }
+            })
+        }
+        catch(e){
+            res.redirect('/');
+            reject('Error while POST token and number')
+        }
+    })
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,33 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
+const flash = require('express-flash');
+const session = require('express-session');
+
+const indexRouter = require('./routes/index'); 
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname+ '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({limit: '10mb', extended:false}));
+app.use('/', indexRouter);
+app.use('/signin', indexRouter);
+app.use('/verify', indexRouter);
+
+app.use((req, res) => {
+})
+
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+
+app.listen(process.env.PORT || 3030);
